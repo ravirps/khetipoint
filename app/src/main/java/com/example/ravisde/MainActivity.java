718bae3aac93e79dcdaae8e74e071e87993ddb0f
unknown <raviand26748@gmail.com>
2021-04-17 21:28:20 +0530

    error handling is done ...next step is to parse and implement listview

--- app/src/main/java/com/example/ravisde/MainActivity.java
+++ app/src/main/java/com/example/ravisde/MainActivity.java
@@ -6,8 +6,13 @@ import androidx.room.Room;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,57 +22,35 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    public SwipeRefreshLayout swipeRefreshLayout ;
-    public ListView list ;
+    SwipeRefreshLayout swipeRefreshLayout ;
+    ListView list ;
     DatabaseClient dbclient;
+    TextView textView;
+    UserDao userDao;
+    String responseData;
+    ProgressBar progressBar;
+    ImageView errorImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         swipeRefreshLayout =  findViewById(R.id.refreshLayout);
         dbclient=DatabaseClient.getInstance(getApplicationContext());
-        UserDao userDao = dbclient.getAppDatabase().userDao();
-
+        userDao = dbclient.getAppDatabase().userDao();
+        progressBar=findViewById(R.id.progress_circular);
+        errorImage=findViewById(R.id.errorImage);
 
         ListView list = findViewById(R.id.list);
 //
-        TextView textView = (TextView) findViewById(R.id.text);
+        textView = findViewById(R.id.text);
 // ...
 
-// Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://private-anon-8a33584439-githubtrendingapi.apiary-mock.com/repositories";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-//                        textView.setText("Response is: "+ response.substring(0,500));
-
-                        List<User> users = userDao.getAll();
-                        if(users!=null)userDao.delete(users.get(0));
-                        User curr=new User();
-                        curr.data=response;
-                        curr.uid=1;
-                        userDao.insertAll(curr);
-                        users = userDao.getAll();
-                        textView.setText(users.get(0).data); 
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
         // Declaring a layout (changes are to be made to this)
         // Declaring a textview (which is inside the layout)
         // Refresh  the layout
@@ -90,11 +73,73 @@ public class MainActivity extends AppCompatActivity {
     }
     private void updateList()
     {
+        if(responseData==null)
+        {
+            showError();
+            return;
+        }
+        progressBar.setVisibility(View.GONE);
+        errorImage.setVisibility(View.GONE);
+        textView.setVisibility(View.VISIBLE);
 
+        textView.setText(responseData);
+//        Log.e("upadting",responseData);
     }
 
     private void refreshData()
     {
 
+        progressBar.setVisibility(View.VISIBLE);
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://private-anon-8a33584439-githubtrendingapi.apiary-mock.com/repositories";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+//                        textView.setText("Response is: "+ response.substring(0,500));
+
+                        List<User> users = userDao.getAll();
+                        if(users!=null)userDao.delete(users.get(0));
+                        User curr=new User();
+                        curr.data=response;
+                        curr.uid=1;
+                        userDao.insertAll(curr);
+                        fetchFromDatabase();
+                        updateList();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"Unable to fetch the data .check your internet connnection", Toast.LENGTH_LONG).show();
+                fetchFromDatabase();
+                updateList();
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    void fetchFromDatabase()
+    {
+        List<User> users= userDao.getAll();
+        if(users==null)
+        {
+            responseData=null;
+            updateList();
+        }
+        responseData=users.get(0).data;
+    }
+    void showError()
+    {
+        progressBar.setVisibility(View.GONE);
+        textView.setVisibility(View.GONE);
+
+        errorImage.setVisibility(View.VISIBLE);
+        Toast.makeText(getApplicationContext(),"Unable to fetch the data .check your internet connnection", Toast.LENGTH_LONG).show();
     }
 }
